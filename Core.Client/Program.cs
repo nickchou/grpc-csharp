ffe6a007d2738e29421daa1044ef86f5d1496133
周黎 <zl6216@ly.com>
2018-04-04 14:45:57 +0800

    grpc client demo

---
+++ Core.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Grpc.Core;
+using Helloworld;
+
+namespace Core.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个长连接的channel（不加密、不安全的）
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+            
+            var client = new Greeter.GreeterClient(channel);
+            String user = "zhangsan";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
